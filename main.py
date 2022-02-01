a1a62224319178ce4613eabe4f4ba9c756441a24
sabiha01 <97167591+sabihask@users.noreply.github.com>
2022-02-01 17:16:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Write your code below this line 👇
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#NEW LINE
+print("Hello world!\nHello World")
+
+#CONCATENATION
+print("Sheikh" + " Sabiha" + " Sultana")
+print("Sheikh" +" " + "Sabiha" + " " + "Sultana")
+
+
+
+
+
+
+
+
+
