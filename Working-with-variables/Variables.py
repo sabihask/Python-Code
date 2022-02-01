11ad5a511c43aba4f19858e891c361084f2ccdbd
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-01 20:00:05 +0000

    Create Variables.py

---
+++ Working-with-variables/Variables.py
@@ -0,0 +1,29 @@
+#CALCULATE LENGHT OF A STRING len()
+print("Enter your name and see it's lenght")
+print(len (input("What is your name? ")))
+print(len("uu"))
+
+####################################
+#PYTHON VARIABLES
+name = input("What is your name?")
+print(name)
+
+
+# INPUT VARIABLES
+print("Input variables to see how python swaps the variables for you")
+a = input("a: ")
+b = input("b: ")
+
+
+####################################
+#Swapping
+c = a
+a = b
+b = c
+
+####################################
+
+print("The swapped variables are:")
+print("a: " + a)
+print("b: " + b)
+
