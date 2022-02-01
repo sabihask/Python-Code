2526181a47cc12f841393a5192494a269d4bca71
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-01 19:11:47 +0000

    Update and rename main.py to concatenation.py

---
+++ Working-with-variables/concatenation.py
@@ -0,0 +1,12 @@
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#NEW LINE
+print("Hello world!\nHello World")
+
+#CONCATENATION
+print("Sheikh" + " Sabiha" + " Sultana")
+print("Sheikh" +" " + "Sabiha" + " " + "Sultana")
+
