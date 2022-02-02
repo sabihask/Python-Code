10db1295e0da704fc4338cc5182d6e92ca15c05d
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-02 15:57:11 +0000

    Create Randint-heads-and-tail-game.py

---
+++ 4 Randomisation-and-Python-list/Randint-heads-and-tail-game.py
@@ -0,0 +1,9 @@
+#Hint: Remember to import the random module first. 🎲
+#random module helps build a lot of games, specially the dice games.
+import random
+# Randint(1,5) will give numbers between 1 and 5 including 1 and 5
+randon_integer=random.randint(0,1)
+if randon_integer == 1:
+  print("It's Tail")
+else:
+  print("It's Heads")
