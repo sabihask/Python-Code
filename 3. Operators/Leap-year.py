a5506c6285281a43872973f51b01eec1d026eddb
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-02 13:16:28 +0000

    Create Leap-year.py

---
+++ 3. Operators/Leap-year.py
@@ -0,0 +1,17 @@
+#Leap year conditon - on every year that is evenly divisible by 4 **except** every year that is evenly divisible by 100 **unless** the year is also evenly divisible by 400
+
+year = int(input("Which year do you want to check? "))
+
+if int(year) % 4 == 0:
+  if int(year) % 100 == 0:
+    if int(year) % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not a leap year")
+  else:
+    print("Not a leap year")
+else:
+  print("Not a leap year")
+
+
+
