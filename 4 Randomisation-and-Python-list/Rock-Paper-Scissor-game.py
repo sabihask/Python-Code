0af74a445d0a76ae3f8d5dd6da5c1833fdf47c71
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-02 16:43:39 +0000

    Create Rock-Paper-Scissor-game.py

---
+++ 4 Randomisation-and-Python-list/Rock-Paper-Scissor-game.py
@@ -0,0 +1,63 @@
+# Make a rock, paper, scissors game.
+# "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."
+# Rock wins against scissors.
+# Scissors win against paper.
+# Paper wins against rock.
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+
+ 
+
+
+
+
+
