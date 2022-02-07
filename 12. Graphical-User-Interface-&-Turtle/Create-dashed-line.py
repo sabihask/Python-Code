56f16f0915d92c64c57da8a2e3ef3c9bcc81165f
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-07 17:39:53 +0000

    Update Create-dashed-line.py

--- 12. Graphical-User-Interface-&-Turtle/Create-dashed-line.py
+++ 12. Graphical-User-Interface-&-Turtle/Create-dashed-line.py
@@ -1,5 +1,8 @@
 import turtle as t
+#Turtle is a special feathers of Python. Using Turtle, we can easily draw in a drawing board.
+#First we import the turtle module. Then create a window, next we create turtle object and using turtle method we can draw in the drawing board.
 
+# create an instance of the class Turtle
 tim = t.Turtle()
 
 ########### Challenge - Draw a Dashed Line ########
