0ce20c7005757ebf5d3cb7d18f875814d8f4c7f3
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-07 15:04:02 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-#Write your code below this line 👇
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-#NEW LINE
-print("Hello world!\nHello World")
+is_on = True
 
-#CONCATENATION
-print("Sheikh" + " Sabiha" + " Sultana")
-print("Sheikh" +" " + "Sabiha" + " " + "Sultana")
-
-
-
-
-
-
-
-
-
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
