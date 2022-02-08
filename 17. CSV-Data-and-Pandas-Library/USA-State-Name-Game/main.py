3b6efd26ae0eaf644ed160eb2c07b3346df57e9f
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-08 16:08:12 +0000

    Add files via upload

---
+++ 17. CSV-Data-and-Pandas-Library/USA-State-Name-Game/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+turtle.shape(image)
+#below function will get us the x and y coordinates but we will use the csv file provided with the course
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_coor())
+#turtle.mainloop()
+
+data = pandas.read_csv("50_states.csv")
+# below we specify from dataframe data we selected column/series state and using method to_list to have a list
+all_states = data.state.to_list()
+guessed_states = []
+print(data)
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
