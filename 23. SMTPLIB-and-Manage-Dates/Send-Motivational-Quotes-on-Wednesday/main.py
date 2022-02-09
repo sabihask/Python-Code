e4c80c4c84318be174a6912bef0f8fc21efc8030
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-09 15:02:18 +0000

    Update main.py

--- 23. SMTPLIB-and-Manage-Dates/Send-Motivational-Quotes-on-Wednesday/main.py
+++ 23. SMTPLIB-and-Manage-Dates/Send-Motivational-Quotes-on-Wednesday/main.py
@@ -8,21 +8,21 @@ with open("quotes.txt") as file:
     data = file.readlines()
     # print(data)
 
-# def send_email():
+def send_email():
 
 
-    # my_email = "sabihasultana.skh@gmail.com"
-    # password = "12341234*"
-    #
-    # with smtplib.SMTP("smtp.gmail.com") as connection:
-    #     connection.starttls()
-    #     connection.login(user=my_email, password=password)
-    #     connection.sendmail(from_addr=my_email, to_addrs="mohiddin.sk@gmail.com", msg=f"Subject:Quote of the week\n\n {quote}")
+    my_email = "SENDER.EMAIL@gmail.com"
+    password = "12341234*"
+    
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="RECIEVER.EMAIL@gmail.com", msg=f"Subject:Quote of the week\n\n {quote}")
 
 now = dt.datetime.now()
 day_of_week = now.weekday()
 
-if day_of_week == 4:
+if day_of_week == 2:
     quote = random.choice(data)
-    # send_email()
+    send_email()
 
