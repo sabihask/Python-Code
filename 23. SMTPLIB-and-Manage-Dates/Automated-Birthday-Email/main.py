05f3ec6135a62d600784112a47381f4b50c23b0f
sabihask <97167591+sabihask@users.noreply.github.com>
2022-02-09 15:54:20 +0000

    Add files via upload

---
+++ 23. SMTPLIB-and-Manage-Dates/Automated-Birthday-Email/main.py
@@ -0,0 +1,33 @@
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
+
+
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "orangesun732@gmail.com"
+MY_PASSWORD = "12341234*"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+# print(data)
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+# print(birthdays_dict)
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+
+
